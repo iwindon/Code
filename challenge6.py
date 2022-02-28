daff3029d3bfad71a299fc67d34bbfa555cf08e7
Ivan Windon <ivan.windon@icloud.com>
2022-02-28 10:07:34 -0500

    adding challenge 6 code

---
+++ challenge6.py
@@ -0,0 +1,16 @@
+name = "Camus"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+answer1 = input("Provide an answer: ")
+answer2 = input("Provide another answer: ")
+
+phrase = "Yesterday I wrote a {}.  I sent it to {}!".format(answer1, answer2)
+print(phrase)
+
+wrong = "aldous Huxley was born in 1894."
+print(wrong.capitalize())
+
