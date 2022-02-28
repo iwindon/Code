dfc77995521d9b22c04f8a9d153edfdc5a8e6df3
Ivan Windon <ivan.windon@icloud.com>
2022-02-28 11:28:28 -0500

    Finished challenge 6 code

--- challenge6.py
+++ challenge6.py
@@ -5,12 +5,43 @@ print(name[2])
 print(name[3])
 print(name[4])
 
-answer1 = input("Provide an answer: ")
-answer2 = input("Provide another answer: ")
+#answer1 = input("Provide an answer: ")
+#answer2 = input("Provide another answer: ")
 
-phrase = "Yesterday I wrote a {}.  I sent it to {}!".format(answer1, answer2)
-print(phrase)
+#phrase = "Yesterday I wrote a {}.  I sent it to {}!".format(answer1, answer2)
+#print(phrase)
 
 wrong = "aldous Huxley was born in 1894."
 print(wrong.capitalize())
+
+splitstring = "Where now? Who now? When now?"
+print(splitstring.split("?"))
+
+fox = ["The", "fox", "Jumped", "over", "the", "fence", "."]
+print(fox)
+fox = " ".join(fox)
+print(fox)
+fox = fox[0:-2] + "."
+print(fox)
+
+change = "A screaming comes across the sky."
+print(change)
+change = change.replace("s", "$")
+print(change)
+
+author = "Hemingway"
+print(author.find("m"))
+
+alice = """\"There is Thingumbob shouting!\" the Bellman said,
+\t\"He is shouting like mad, only hark!
+He is waving his hands, he is wagging his head,
+\tHe has certainly found a Snark!\""""
+print(alice)
+
+word1 = "three "
+print(word1 + word1  + word1)
+print(word1 * 3)
+
+phrase1 = "It was a bright cold day in April, and the clocks were striking thirteen."
+print(phrase1[:33])
 
